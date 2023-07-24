a7cdc5fdbb4468344a1a9b8a99f2e017222170be
BoomerRP <125202308+BoomerRP@users.noreply.github.com>
2023-07-24 19:22:29 -0300

    feat: map the foods in the createOrder

--- client/src/pages/ERP/TakeOrder.jsx
+++ client/src/pages/ERP/TakeOrder.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Button } from '@rewind-ui/core'
+import { useFood } from '../../context/FoodContext'
+import foodModel from '../../../../src/models/food.model'
 
 const TakeOrder = () => {
+
+   const  { foods, getFoods } = useFood()
+
+   useEffect(() => {
+     getFoods()
+   }, [])
+   
+
   return (
     <>  
     <div className='bg-zinc-700 h-full'>
@@ -10,17 +20,13 @@ const TakeOrder = () => {
             
             <div className='flex flex-row w-4/6 m-5 flex-wrap items-center   gap-5'>
                 <div className='border-2 rounded h-20 w-20 text-center my-auto'>cheese <br /> burger</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto'  >doble <br /> cheese</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >clasica</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >clasica <br /> doble</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >bacon <br /> simple</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >bacon <br /> doble</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >garompa</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >garompa <br /> doble</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >bacon <br /> simple</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >bacon <br /> doble</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >garompa</div>
-                <div className='border-2 rounded h-20 w-20 text-center my-auto' >garompa <br /> doble</div>
+                {
+                    foods.map(food => (
+                        <div key={food._id}  className='border-2 rounded h-20 w-20 text-center my-auto'>
+                            <p>{food.name}</p>
+                        </div>
+                    ))
+                }
             </div>
             <div className='flex flex-col mx-auto'>
                 <div>

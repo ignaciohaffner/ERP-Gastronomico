a995a4613d332aff596b78b6c35df2deda26766c
BoomerRP <ignaciohaffner@gmail.com>
2024-03-05 01:30:26 -0300

    Fix: Remove imports.

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,67 +1,71 @@
-import { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
-import { useAuth } from '../context/AuthContext'
-import { Link, useNavigate } from 'react-router-dom'
-import { Combobox } from '@rewind-ui/core';
-
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { useAuth } from "../context/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
-    const { register, handleSubmit, formState: { errors } } = useForm()
+  const { signin, errors: signInErrors, isAuthenticated } = useAuth();
+  const onSubmit = handleSubmit((data) => {
+    signin(data);
+  });
 
-    const { signin, errors: signInErrors, isAuthenticated } = useAuth()
-    const onSubmit = handleSubmit(data => {
-        signin(data)
+  const navigate = useNavigate();
 
-    })
+  useEffect(() => {
+    if (isAuthenticated) navigate("/");
+  }, [isAuthenticated]);
 
-    const navigate = useNavigate()
+  return (
+    <div className="flex h-[calc(100vh-100px)] items-center justify-center">
+      <div className="bg-zinc-800 max-w-md w-full p-10 rounded-md">
+        {signInErrors.map((error, i) => (
+          <div key={i} className="bg-red-500 p-2 my-2 text-center text-white ">
+            {error}
+          </div>
+        ))}
+        <h1 className="text-3xl font-bold mb-5">Login</h1>
 
-    useEffect(() => {
-        if (isAuthenticated) navigate('/')
-    }, [isAuthenticated])
+        <form onSubmit={onSubmit}>
+          <input
+            type="email"
+            {...register("email", { required: true })}
+            placeholder="Email"
+            className="w-full mb-5 bg-zinc-700 text-white px-4 py-2 rounded-md"
+          />
+          {errors.email && <p className="text-red-500">Email is required</p>}
+          <input
+            type="password"
+            {...register("password", { required: true })}
+            placeholder="Password"
+            className="w-full mb-2 bg-zinc-700 text-white px-4 py-2 rounded-md"
+          />
+          {errors.password && (
+            <p className="text-red-500">Password is required</p>
+          )}
 
+          <button
+            type="submit"
+            className=" bg-sky-500 text-white px-4 py-2 rounded-md my-2 "
+          >
+            Login
+          </button>
+        </form>
+        <p className="flex gap-x-2 justify-between">
+          ¿No tienes una cuenta?{" "}
+          <Link to="/register" className="text-sky-500">
+            {" "}
+            Registrate
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div className='flex h-[calc(100vh-100px)] items-center justify-center'>
-            <div className='bg-zinc-800 max-w-md w-full p-10 rounded-md'>
-                {
-                    signInErrors.map((error, i) => (
-                        <div key={i} className='bg-red-500 p-2 my-2 text-center text-white '>
-                            {error}
-                        </div>
-                    ))
-                }
-                <h1 className='text-3xl font-bold mb-5'>Login</h1>
-
-                <form onSubmit={onSubmit}>
-                    <input type="email" {
-                        ...register('email', { required: true })
-                    } placeholder="Email" className="w-full mb-5 bg-zinc-700 text-white px-4 py-2 rounded-md" />
-                    {
-                        errors.email && (
-                            <p className='text-red-500'>Email is required</p>
-                        )
-                    }
-                    <input type="password" {
-                        ...register('password', { required: true })
-                    } placeholder="Password" className="w-full mb-2 bg-zinc-700 text-white px-4 py-2 rounded-md" />
-                    {
-                        errors.password && (
-                            <p className='text-red-500'>Password is required</p>
-                        )
-                    }
-
-                    <button type='submit' className=' bg-sky-500 text-white px-4 py-2 rounded-md my-2 '>
-                        Login
-                    </button>
-                </form>
-                <p className='flex gap-x-2 justify-between'>
-                    ¿No tienes una cuenta? <Link to="/register" className='text-sky-500'> Registrate</Link>
-                </p>
-            </div>
-        </div>
-    )
-}
-
-export default LoginPage
+export default LoginPage;

5b47ef46441a01560873136bdffde7d6cba7d214
BoomerRP <ignaciohaffner@gmail.com>
2024-03-05 02:07:06 -0300

    fix: change console.log

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
 export const connectdb = async () => {
-    console.log('intento conectar a la bdd')
-    try {
-        console.log('antes del await')
-        await mongoose.connect('mongodb+srv://ignaciohaffner:Dy7DvTzBL7M8Kdrg@cluster0.vyyfv8z.mongodb.net/?retryWrites=true&w=majority')
-        console.log(">>> DB CONECTADA MOSTRO")
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
+  console.log("intento conectar a la bdd");
+  try {
+    console.log("antes del await");
+    await mongoose.connect(
+      "mongodb+srv://ignaciohaffner:Dy7DvTzBL7M8Kdrg@cluster0.vyyfv8z.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log(">>> DB CONECTADA");
+  } catch (error) {
+    console.log(error);
+  }
+};
